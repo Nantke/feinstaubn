8a2e6dae1ec0b9c4d4f8c022880fc76369b86436
Janssen, Nantke <nantke.janssen@goslar.de>
2025-02-20 13:53:47 +0100

    back

--- FeinstaubFigmaDesign2/vite.config.js
+++ FeinstaubFigmaDesign2/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.umweltbundesamt.de',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
-
